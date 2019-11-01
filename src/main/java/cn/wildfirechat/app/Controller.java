4601c2cf28cc15661325c4e3fca2791526cb0656
imndx <imndx@gmail.com>
2019-11-01 10:39:05 +0800

    PC登录相关的两个接口，允许跨域访问

--- src/main/java/cn/wildfirechat/app/Controller.java
+++ src/main/java/cn/wildfirechat/app/Controller.java
@@ -14,12 +14,12 @@ public class Controller {
         return "Ok";
     }
 
-    @PostMapping(value = "/send_code", produces = "application/json;charset=UTF-8"   )
+    @PostMapping(value = "/send_code", produces = "application/json;charset=UTF-8")
     public Object sendCode(@RequestBody SendCodeRequest request) {
         return mService.sendCode(request.getMobile());
     }
 
-    @PostMapping(value = "/login", produces = "application/json;charset=UTF-8"   )
+    @PostMapping(value = "/login", produces = "application/json;charset=UTF-8")
     public Object login(@RequestBody LoginRequest request) {
         return mService.login(request.getMobile(), request.getCode(), request.getClientId());
     }
@@ -29,12 +29,14 @@ public class Controller {
     1, PC -> App     创建会话
     2, PC -> App     轮询调用session_login进行登陆，如果已经扫码确认返回token，否则反正错误码9（已经扫码还没确认)或10(还没有被扫码)。
      */
-    @PostMapping(value = "/pc_session", produces = "application/json;charset=UTF-8"   )
+    @CrossOrigin
+    @PostMapping(value = "/pc_session", produces = "application/json;charset=UTF-8")
     public Object createPcSession(@RequestBody CreateSessionRequest request) {
         return mService.createPcSession(request);
     }
 
-    @PostMapping(value = "/session_login/{token}", produces = "application/json;charset=UTF-8"   )
+    @CrossOrigin
+    @PostMapping(value = "/session_login/{token}", produces = "application/json;charset=UTF-8")
     public Object loginWithSession(@PathVariable("token") String token) {
         return mService.loginWithSession(token);
     }
@@ -43,12 +45,12 @@ public class Controller {
     1，扫码，调用/scan_pc接口。
     2，调用/confirm_pc 接口进行确认
      */
-    @PostMapping(value = "/scan_pc/{token}", produces = "application/json;charset=UTF-8"   )
+    @PostMapping(value = "/scan_pc/{token}", produces = "application/json;charset=UTF-8")
     public Object scanPc(@PathVariable("token") String token) {
         return mService.scanPc(token);
     }
 
-    @PostMapping(value = "/confirm_pc", produces = "application/json;charset=UTF-8"   )
+    @PostMapping(value = "/confirm_pc", produces = "application/json;charset=UTF-8")
     public Object confirmPc(@RequestBody ConfirmSessionRequest request) {
         return mService.confirmPc(request);
     }
@@ -57,7 +59,7 @@ public class Controller {
     /*
      * 本工程为demo应用，没有鉴权机制，谁都可以调用。正式商用需要加上鉴权，防止被攻击
      */
-    @PostMapping(value = "/put_group_announcement", produces = "application/json;charset=UTF-8"   )
+    @PostMapping(value = "/put_group_announcement", produces = "application/json;charset=UTF-8")
     public Object putGroupAnnouncement(@RequestBody GroupAnnouncementPojo request) {
         return mService.putGroupAnnouncement(request);
     }
@@ -65,7 +67,7 @@ public class Controller {
     /*
      * 本工程为demo应用，没有鉴权机制，这里这里请求需要带上请求用户。
      */
-    @PostMapping(value = "/get_group_announcement", produces = "application/json;charset=UTF-8"   )
+    @PostMapping(value = "/get_group_announcement", produces = "application/json;charset=UTF-8")
     public Object getGroupAnnouncement(@RequestBody GroupIdPojo request) {
         return mService.getGroupAnnouncement(request.groupId);
     }
