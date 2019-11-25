292630c8a8f29fc44ea14c743e777c2493ae190d
heavyrain2012 <heavyrain.lee@gmail.com>
2019-11-25 12:46:21 +0800

    update

--- src/main/java/cn/wildfirechat/app/pojo/SessionOutput.java
+++ src/main/java/cn/wildfirechat/app/pojo/SessionOutput.java
@@ -4,16 +4,18 @@ public class SessionOutput {
     private String token;
     private int status;
     private long expired;
+    private int platform;
     private String device_name;
 
     public SessionOutput() {
     }
 
-    public SessionOutput(String token, int status, long expired, String device_name) {
+    public SessionOutput(String token, int status, long expired, String device_name, int platform) {
         this.token = token;
         this.status = status;
         this.expired = expired;
         this.device_name = device_name;
+        this.platform = platform;
     }
 
     public String getToken() {
@@ -46,5 +48,13 @@ public class SessionOutput {
 
     public void setDevice_name(String device_name) {
         this.device_name = device_name;
+    }
+
+    public int getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(int platform) {
+        this.platform = platform;
     }
 }
