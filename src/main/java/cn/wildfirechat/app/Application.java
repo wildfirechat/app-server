7d9e0a56579d1d2a9062bb29ee3aa3b0a7693a5b
heavyrain.lee <heavyrain.lee@gmail.com>
2020-02-26 21:18:12 +0800

    修正上传文件超大的错误

--- src/main/java/cn/wildfirechat/app/Application.java
+++ src/main/java/cn/wildfirechat/app/Application.java
@@ -2,10 +2,29 @@ package cn.wildfirechat.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+
+	/**
+	 * 文件上传配置
+	 * @return
+	 */
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		//单个文件最大
+		factory.setMaxFileSize("20480KB"); //20MB
+		/// 设置总上传数据总大小
+		factory.setMaxRequestSize("102400KB");
+		return factory.createMultipartConfig();
 	}
 }
