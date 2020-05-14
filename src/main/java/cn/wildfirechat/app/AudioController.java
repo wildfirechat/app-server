0736f10c157a88b538d52da0fedc32105eb8e082
imndx <imndxx@gmail.com>
2020-05-14 17:45:15 +0800

    amr转mp3

---
+++ src/main/java/cn/wildfirechat/app/AudioController.java
@@ -0,0 +1,107 @@
+package cn.wildfirechat.app;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ws.schild.jave.*;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+@RestController
+public class AudioController {
+
+    @Value("${wfc.audio.cache.dir}")
+    String cacheDirPath;
+    private File cacheDir;
+    private MessageDigest md;
+
+    @PostConstruct
+    public void init() throws NoSuchAlgorithmException {
+        cacheDir = new File(cacheDirPath);
+        if (!cacheDir.exists()) {
+            cacheDir.mkdirs();
+        }
+        md = MessageDigest.getInstance("MD5");
+    }
+
+    @GetMapping("amr2mp3")
+    public CompletableFuture<ResponseEntity<InputStreamResource>> amr2mp3(@RequestParam("path") String amrUrl) throws FileNotFoundException {
+
+        MediaType mediaType = new MediaType("application", "octet-stream");
+        byte[] messageDigest = md.digest(amrUrl.getBytes());
+        BigInteger number = new BigInteger(1, messageDigest);
+        String mp3FileName = number.toString() + ".mp3";
+
+        File mp3File = new File(cacheDir, mp3FileName);
+        if (mp3File.exists()) {
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(mp3File));
+            return CompletableFuture.completedFuture(ResponseEntity.ok()
+//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + mp3File.getName())
+                .contentType(mediaType)
+                .contentLength(mp3File.length())
+                .body(resource));
+        }
+
+        return CompletableFuture.supplyAsync(new Supplier<ResponseEntity<InputStreamResource>>() {
+            /**
+             * Gets a result.
+             *
+             * @return a result
+             */
+            @Override
+            public ResponseEntity<InputStreamResource> get() {
+
+                try {
+                    amr2mp3(amrUrl, mp3File);
+                    InputStreamResource resource = new InputStreamResource(new FileInputStream(mp3File));
+                    return ResponseEntity.ok()
+//                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + mp3File.getName())
+                        .contentType(mediaType)
+                        .contentLength(mp3File.length())
+                        .body(resource);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (EncoderException e) {
+                    e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                return ResponseEntity.status(404).build();
+            }
+        });
+    }
+
+    private static void amr2mp3(String sourceUrl, File target) throws MalformedURLException, EncoderException {
+        //Audio Attributes
+        AudioAttributes audio = new AudioAttributes();
+        audio.setCodec("libmp3lame");
+        audio.setBitRate(128000);
+        audio.setChannels(2);
+        audio.setSamplingRate(44100);
+
+        //Encoding attributes
+        EncodingAttributes attrs = new EncodingAttributes();
+        attrs.setFormat("mp3");
+        attrs.setAudioAttributes(audio);
+
+        //Encode
+        Encoder encoder = new Encoder();
+        encoder.encode(new MultimediaObject(new URL(sourceUrl)), target, attrs);
+
+    }
+
+}
