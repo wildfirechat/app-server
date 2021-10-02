7723a6ded691bd7dba9103e0a637ea85c3a3e3b3
heavyrain2012 <heavyrain.lee@gmail.com>
2021-10-02 17:25:13 +0800

    对讲功能支持

---
+++ src/main/java/cn/wildfirechat/app/pojo/PttChannelInfo.java
@@ -0,0 +1,11 @@
+package cn.wildfirechat.app.pojo;
+
+public class PttChannelInfo {
+    public String channelId;
+    public String channelTitle;
+    public String channelDesc;
+    public String password;
+    public String pin;
+    public String owner;
+    public boolean open;
+}
---
+++ src/main/java/cn/wildfirechat/app/pojo/PttChannelInfoRequest.java
@@ -0,0 +1,6 @@
+package cn.wildfirechat.app.pojo;
+
+public class PttChannelInfoRequest {
+    public String channelId;
+    public String password;
+}
