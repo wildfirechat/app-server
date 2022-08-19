70161335e34815751c0eafb79747b02086c7044e
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-08-19 09:21:43 +0800

    add build release script

---
+++ build_release.sh
@@ -0,0 +1,11 @@
+if [ $# -eq 0 ]; then
+    echo "Usage: sh build_release.sh version"
+    exit -1
+fi
+echo "build release $1"
+
+mvn clean package
+cd target
+cp -af ../config ./
+cp -af release_note.md ./
+tar -xzvf app-server-release-$1.tar.gz app-$1.jar config release_note.md
