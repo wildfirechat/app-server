7ef738b583c8c51563ac7a7a703c637ddddd95a4
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-10-23 21:37:29 +0800

    更新发布版本脚本

--- build_release.sh
+++ build_release.sh
@@ -7,6 +7,9 @@ echo "build release $1"
 mvn clean package
 cd target
 cp -af ../config ./
+cp -af ../systemd ./
 cp -af ../release_note.md ./
-tar -czvf app-server-release-$1.tar.gz app-$1.jar config release_note.md
+tar -czvf app-server-release-$1.tar.gz app-$1.jar config systemd release_note.md
 cp app-server-release-$1.tar.gz app-server-release-latest.tar.gz
+cp app-server-0.61.rpm app-server-latest.rpm
+cp app-server-0.61.deb app-server-latest.deb
