6ecd6612a51f0031860af3d572dc8dbecb7fd342
imndx <imndxx@gmail.com>
2022-12-18 12:15:34 +0800

    修复清理pc session 失败

--- src/main/java/cn/wildfirechat/app/jpa/PCSessionRepository.java
+++ src/main/java/cn/wildfirechat/app/jpa/PCSessionRepository.java
@@ -1,8 +1,12 @@
 package cn.wildfirechat.app.jpa;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PCSessionRepository extends CrudRepository<PCSession, String> {
 
+    @Modifying
+    @Transactional
     Long deleteByCreateDtBefore(long timestamp);
 }
