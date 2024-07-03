b2656928a586c0a6749c6a3fb033484285c1b32c
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2024-07-03 10:59:32 +0800

    remove useless log

--- src/main/java/cn/wildfirechat/app/shiro/ShiroSessionManager.java
+++ src/main/java/cn/wildfirechat/app/shiro/ShiroSessionManager.java
@@ -24,8 +24,6 @@ public class ShiroSessionManager extends DefaultWebSessionManager {
     protected Serializable getSessionId(ServletRequest request, ServletResponse response){
         String id = WebUtils.toHttp(request).getHeader(AUTHORIZATION);
         if(StringUtils.isEmpty(id)){
-            //如果没有携带id参数则按照父类的方式在cookie进行获取
-            System.out.println("super："+super.getSessionId(request, response));
             return super.getSessionId(request, response);
         }else{
             //如果请求头中有 authToken 则其值为sessionId
