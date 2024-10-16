78f02aa548fa176c9e9ba6c0d4dd8872dc1251c5
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2024-10-16 22:31:50 +0800

    扫码登录确认接口不需要im token

--- src/main/java/cn/wildfirechat/app/pojo/ConfirmSessionRequest.java
+++ src/main/java/cn/wildfirechat/app/pojo/ConfirmSessionRequest.java
@@ -1,20 +1,11 @@
 package cn.wildfirechat.app.pojo;
 
 public class ConfirmSessionRequest {
-    private String im_token;
     private String token;
     private String user_id;
     private int quick_login;
 
     //{"token":"22295ee9-d4e3-4fc0-bda3-bcfe008dce08","user_id":"CeDRCRtt","quick_login":true}
-    public String getIm_token() {
-        return im_token;
-    }
-
-    public void setIm_token(String im_token) {
-        this.im_token = im_token;
-    }
-
     public String getToken() {
         return token;
     }
