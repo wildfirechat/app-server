7b144421c3f2e5f6a107b07546ef9fc7ddd6066f
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2025-05-16 09:50:46 +0800

    no message

--- src/main/java/cn/wildfirechat/app/tools/Utils.java
+++ src/main/java/cn/wildfirechat/app/tools/Utils.java
@@ -1,6 +1,8 @@
 package cn.wildfirechat.app.tools;
 
+import java.nio.file.Paths;
 import java.util.Random;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,4 +26,28 @@ public class Utils {
         return flag;
     }
 
+    public static String getSafeFileName(String fileName) {
+        if (fileName == null || fileName.isEmpty()) {
+            return UUID.randomUUID().toString();
+        }
+
+        // 使用 Paths.get 解析文件名
+        try {
+            String newName = Paths.get(fileName).getFileName().toString();
+            if(!newName.isEmpty()) {
+                return newName;
+            }
+        } catch (Exception e) {
+            // 处理解析异常
+            e.printStackTrace();
+        }
+        return UUID.randomUUID().toString();
+    }
+
+    public static void main(String[] args) {
+        String filename1 = "/aa../../../hello.txt";
+        String filename2 = "..\\..\\1.txt";
+        System.out.println(getSafeFileName(filename1));
+        System.out.println(getSafeFileName(filename2));
+    }
 }
