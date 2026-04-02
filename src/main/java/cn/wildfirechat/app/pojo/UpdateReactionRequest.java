f8da8bc50e95d366763a74136f2f55dbb881fa63
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2026-04-02 16:26:32 +0800

    messageUid使用string

--- src/main/java/cn/wildfirechat/app/pojo/UpdateReactionRequest.java
+++ src/main/java/cn/wildfirechat/app/pojo/UpdateReactionRequest.java
@@ -1,14 +1,19 @@
 package cn.wildfirechat.app.pojo;
 
 public class UpdateReactionRequest {
-    private long messageUid;
+    // 使用 String 接收，避免 JSON 解析 long 类型的问题
+    private String messageUid;
     private String emoji;
 
     public long getMessageUid() {
-        return messageUid;
+        try {
+            return Long.parseLong(messageUid);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 
-    public void setMessageUid(long messageUid) {
+    public void setMessageUid(String messageUid) {
         this.messageUid = messageUid;
     }
 
